335d3bea902cc88f7028db19b0b8eadb42cb4e73
Christopher Fuller <christopher.fuller@gotinder.com>
2021-07-01 12:37:56 -0700

    Update Swift dependencies

--- Package.swift
+++ Package.swift
@@ -1,17 +1,24 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.4
 
 import PackageDescription
 
 let package = Package(
     name: "StateMachine",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v5),
+    ],
     products: [
         .library(
             name: "StateMachine",
             targets: ["StateMachine"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble.git",
-                 from: "9.0.0-rc.3"),
+        .package(
+            url: "https://github.com/Quick/Nimble.git",
+            from: "9.2.0"),
     ],
     targets: [
         .target(
