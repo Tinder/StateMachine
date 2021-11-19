6a493bf110ec25e9d7388e61f1adaebe604b8131
Christopher Fuller <christopher.fuller@gotinder.com>
2021-11-19 10:54:36 -0800

    Add Observer type as workaround for Swift bug SR-12294 (#46)
    * Use Xcode 12.5.1
    
    * Add Observer type as workaround for Swift bug SR-12294

--- Swift/Sources/StateMachine/StateMachine.swift
+++ Swift/Sources/StateMachine/StateMachine.swift
@@ -27,6 +27,13 @@ open class StateMachine<State: StateMachineHashable, Event: StateMachineHashable
         public struct Invalid: Error, Equatable {}
     }
 
+    private struct Observer {
+
+        weak var object: AnyObject?
+
+        let callback: Transition.Callback
+    }
+
     public typealias Definition = StateMachineTypes.Definition<State, Event, SideEffect>
 
     private typealias DefinitionBuilder = StateMachineTypes.DefinitionBuilder
@@ -42,7 +49,7 @@ open class StateMachine<State: StateMachineHashable, Event: StateMachineHashable
     public private(set) var state: State
 
     private let states: States
-    private var observers: [(object: () -> AnyObject?, callback: Transition.Callback)] = []
+    private var observers: [Observer] = []
 
     public init(@DefinitionBuilder build: () -> Definition) {
         let definition: Definition = build()
@@ -52,8 +59,8 @@ open class StateMachine<State: StateMachineHashable, Event: StateMachineHashable
                 $0[$1.event] = $1.action
             }
         }
-        observers = definition.callbacks.map { [unowned self] in
-            (object: { self }, callback: $0)
+        observers = definition.callbacks.map {
+            Observer(object: self, callback: $0)
         }
     }
 
@@ -61,7 +68,7 @@ open class StateMachine<State: StateMachineHashable, Event: StateMachineHashable
     public func startObserving(_ observer: AnyObject?, callback: @escaping Transition.Callback) -> Self {
         guard let observer: AnyObject = observer
         else { return self }
-        observers.append((object: { [weak observer] in observer }, callback: callback))
+        observers.append(Observer(object: observer, callback: callback))
         return self
     }
 
@@ -71,7 +78,7 @@ open class StateMachine<State: StateMachineHashable, Event: StateMachineHashable
 
     public func stopObserving(_ observers: [AnyObject?]) {
         self.observers.removeAll {
-            guard let object: AnyObject = $0.object()
+            guard let object: AnyObject = $0.object
             else { return true }
             return observers.contains { $0 === object }
         }
@@ -104,9 +111,9 @@ open class StateMachine<State: StateMachineHashable, Event: StateMachineHashable
     }
 
     private func notify(_ result: Transition.Result) {
-        var observers: [(object: () -> AnyObject?, callback: Transition.Callback)] = []
+        var observers: [Observer] = []
         for observer in self.observers {
-            guard observer.object() != nil
+            guard observer.object != nil
             else { continue }
             observers.append(observer)
             observer.callback(result)
